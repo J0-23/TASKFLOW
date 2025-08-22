59f372c2feb636a08162daea168451d0af17faaa
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-08-22 15:55:30 +0100

    backend

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -53,5 +53,88 @@ export const getTask = asyncHandler(async (req, res) => {
     const userId = req.user._id;
 
     const { id } = req.params;
-  } catch (error) {}
+
+    if (!id) {
+      res.status(400).json({ message: "Please provide a task ID" });
+    }
+
+    const task = await TaskModel.findById(id);
+
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+    }
+
+    if (!task.user.equals(userId)) {
+      res.status(401).json({ message: "No permission to view this task" });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.log("Error in getTask: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export const updateTask = asyncHandler(async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const { id } = req.params;
+    const { title, description, dueDate, priority, status, completed } =
+      req.body;
+
+    if (!id) {
+      res.status(400).json({ message: "Please provide a task id" });
+    }
+
+    const task = await TaskModel.findById(id);
+
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+    }
+    // check if user is owner of the task
+    if (!task.user.equals(userId)) {
+      res.status(401).json({ message: "No permission to view this task" });
+    }
+
+    // update task with new data if provided or keep unchanged
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.dueDate = dueDate || task.dueDate;
+    task.priority = priority || task.priority;
+    task.status = status || task.status;
+    task.completed = completed || task.completed;
+
+    await task.save();
+
+    return res.status(200).json(task);
+  } catch (error) {
+    console.log("Error in updateTask: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export const deleteTask = asyncHandler(async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id } = req.params;
+
+    const task = await TaskModel.findById(id);
+
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+    }
+
+    // check if user is owner of the task
+    if (!task.user.equals(userId)) {
+      res.status(401).json({ message: "No permission to view this task" });
+    }
+
+    await TaskModel.findByIdAndDelete(id);
+
+    return res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteTask: ", error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
