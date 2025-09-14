daf8c16d6eb199dd8ab3c17c9474ea3bf5b14441
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-09-14 19:23:01 +0100

    cors fix

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -43,8 +43,8 @@ export const registerUser = asyncHandler(async (req, res) => {
     path: "/",
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite: true,
-    secure: true,
+    sameSite: "none",
+    secure: false,
   });
 
   if (user) {
@@ -103,8 +103,8 @@ export const loginUser = asyncHandler(async (req, res) => {
       path: "/",
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      sameSite: true,
-      secure: true,
+      sameSite: "none",
+      secure: false,
     });
 
     // send back the user and token
@@ -127,7 +127,12 @@ export const loginUser = asyncHandler(async (req, res) => {
 
 // logout user
 export const logoutUser = asyncHandler(async (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: false,
+    path: "/",
+  });
 
   res.status(200).json({ message: "User logged out" });
 });
